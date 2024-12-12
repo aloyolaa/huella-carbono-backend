057b21cb35f0fe50d37fe477dd45c7535822dd98
Alan Loyola <alanloyola2002@gmail.com>
2024-12-12 15:29:49 -0500

    Initial commit

---
+++ src/main/java/com/towers/huellacarbonobackend/HuellaCarbonoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.towers.huellacarbonobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuellaCarbonoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuellaCarbonoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/towers/huellacarbonobackend/HuellaCarbonoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.towers.huellacarbonobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuellaCarbonoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
