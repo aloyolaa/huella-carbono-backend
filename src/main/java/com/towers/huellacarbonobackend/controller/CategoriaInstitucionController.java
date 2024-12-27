6fa4a944686db4949194ae01bd0124ce10e0863f
Alan Loyola <alanloyola2002@gmail.com>
2024-12-26 19:36:38 -0500

    Refactor CategoriaInstitucionController

--- src/main/java/com/towers/huellacarbonobackend/controller/CategoriaInstitucionController.java
+++ src/main/java/com/towers/huellacarbonobackend/controller/CategoriaInstitucionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/categoria-institución")
+@RequestMapping("/categoria-institucion")
 @RequiredArgsConstructor
 public class CategoriaInstitucionController {
     private final CategoriaInstitucionService categoriaInstitucionService;
